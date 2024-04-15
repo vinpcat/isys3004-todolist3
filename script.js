77ad0d9a910434b44f0a9d5b6dd224bf24014971
Vincent Catalan <126637057+vinpcat@users.noreply.github.com>
2024-04-15 09:33:17 +0800

    ..

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Empty script file to start with
+
+
+// 1. Assgin variables to DOM elemetns
+let addtaskbutton = document.getElementById("add-button");
+let newtaskinput = document.getElementById("task-input");
+let todocontainer = document.getElementById("todo-list");
+let templateelement = document.getelementbydid("todo-item-template")
+let template = templateelement.innerhtml
+
+// 2. Write function to handle the wevents
+function onaddtaskclicked(event){
+    lettaskname = newtaskinput.ariaValueMax;
+    newtaskinput.value = ""
+
+
+    let todohtml = template.replace("<!-- Task_Name -->", taskname)
+    todoContainer.insertAdjacentHTML('afterbegin',taskname)
+}
+
+// 3. link,element, event to the function
+addtaskbutton.addEventListener('click', onaddtaskclicked)
